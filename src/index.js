fb2573c55b396118471ac2b637ef4806b0e549b2
Francisca Principe <franciscaprincipe0@gmail.com>
2025-03-13 16:29:40 -0400

    Improved the UX and added more css to poem generator.

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
+    let poemElement =document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class=" generating"> *** Generating a poem just for you about ${instructionsInput.value}...</div>`
+
     axios.get(apiUrl).then(displayPoem);
 }
 
